3cb592237206bb3403214ba4299d8c1303a7956e
David Parr <parr758@googlemail.com>
2026-08-12 15:47:01 +0100

    feature: Add get_secret function and tests.

---
+++ src/utils.py
@@ -0,0 +1,24 @@
+import boto3
+import json
+from botocore.exceptions import ClientError
+
+
+def get_secret(secret_name):
+
+    region_name = "eu-west-2"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+        return json.loads(get_secret_value_response["SecretString"])
+
+    except ClientError as e:
+        raise e
