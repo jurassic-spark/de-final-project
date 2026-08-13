ed5e2318f2d2172fe78883b399468fbdf5737ae5
Mohammedal-abdulhamid <mohammedalabdulhamid@gmail.com>
2026-08-13 14:52:22 +0100

    save dta to s3 & test it

--- tests/test_extract_data.py
+++ tests/test_extract_data.py
@@ -2,9 +2,14 @@ import pytest
 import pandas as pd
 from unittest import mock
 from psycopg2 import OperationalError
-
 from pandas.errors import DatabaseError
 from ingestion.extract_data import extract_data
+import boto3
+import pytest
+from moto import mock_aws
+from pandas.testing import assert_frame_equal 
+
+
 
 
 @mock.patch("ingestion.extract_data.pd.read_sql_query")
@@ -51,4 +56,21 @@ def test_extract_data_raises_exception():
 
 
 
+
+@mock_aws
+def save_dataframe_to_s3_parquet():
+    # arrange
+    s3_client = boto3.client("s3", region_name="us-east-1")
+    # create a bucket
+    s3_client.create_bucket(Bucket="mock_s3")
+    mock_data = {
+            "secrets":"secrets",
+            "table_name":"sales_order",
+            "timestamp":"2026-01-01 00:00:00"
+    }
+    mock_df = pd.DataFrame(mock_data )
+    filepath = save_dataframe_to_s3_parquet(mock_df, "mock_s3", "sales_order")
+    saved_df = pd.read_parquet(path=f's3://mock_s3/{filepath}')
+
+    assert pd.testing.assert_frame_equal(mock_df,saved_df )
 
