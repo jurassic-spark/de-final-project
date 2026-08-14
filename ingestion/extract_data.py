a5bfbffd1c46dd7b0ef42be6944ac0118927cd10
johnnybquintero-ui <johnnybquintero@gmail.com>
2026-08-14 16:07:56 +0100

    Clean up ingestion code and remove placeholder

--- ingestion/extract_data.py
+++ ingestion/extract_data.py
@@ -12,25 +12,22 @@ import json
 from botocore.exceptions import ClientError
 from datetime import datetime, timedelta
 
+
 def get_secret(secret_name):
 
     region_name = "eu-west-2"
 
     # Create a Secrets Manager client
     session = boto3.session.Session()
-    client = session.client(
-        service_name='secretsmanager',
-        region_name=region_name
-    )
+    client = session.client(service_name="secretsmanager", region_name=region_name)
 
     try:
-        get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
-        )
+        get_secret_value_response = client.get_secret_value(SecretId=secret_name)
         return json.loads(get_secret_value_response["SecretString"])
 
     except ClientError as e:
         raise e
+
 
 def extract_data(secrets, table_name, timestamp):
     username = secrets["username"]
@@ -39,7 +36,9 @@ def extract_data(secrets, table_name, timestamp):
     host = secrets["host"]
     port = secrets["port"]
 
-    connection = psycopg2.connect(user=username, password=password, dbname=db_name, host=host, port=port)
+    connection = psycopg2.connect(
+        user=username, password=password, dbname=db_name, host=host, port=port
+    )
 
     cur = connection.cursor()
     if timestamp == None:
@@ -54,11 +53,15 @@ def extract_data(secrets, table_name, timestamp):
     cur.close()
     connection.close()
 
-    data_string = [tuple(str(col) if hasattr(col, "isoformat") else col for col in row)for row in rows]
+    data_string = [
+        tuple(str(col) if hasattr(col, "isoformat") else col for col in row)
+        for row in rows
+    ]
     df = pd.DataFrame(data_string, columns=columns)
 
     print("dataframe:", table_name)
     return df
+
 
 def save_dataframe_to_s3_parquet(dataframe, bucket_name, table_name):
     parquet_buffer = BytesIO()
@@ -67,23 +70,20 @@ def save_dataframe_to_s3_parquet(dataframe, bucket_name, table_name):
 
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 
-    object_key = f"raw/{table_name}/{table_name}_{timestamp}.parquet" #Potential specificity using timestamps here
+    object_key = f"raw/{table_name}/{table_name}_{timestamp}.parquet"  # Potential specificity using timestamps here
 
     s3_client = boto3.client("s3")
 
     s3_client.put_object(
-    Bucket=bucket_name,
-    Key=object_key,
-    Body=parquet_buffer.getvalue()
-)
+        Bucket=bucket_name, Key=object_key, Body=parquet_buffer.getvalue()
+    )
 
     return object_key
 
+
 def lambda_handler(event, context):
     s3_client = boto3.client("s3")
-    ingest_objects = s3_client.list_objects_v2(
-        Bucket=os.environ["INGEST_BUCKET"]
-    )
+    ingest_objects = s3_client.list_objects_v2(Bucket=os.environ["INGEST_BUCKET"])
 
     if ingest_objects.get("KeyCount", 0) > 0:
         timestamp = datetime.now() - timedelta(minutes=30)
@@ -91,24 +91,24 @@ def lambda_handler(event, context):
         timestamp = None
 
     secret = get_secret("totesys_database_credentials")
-    
-    known_tables = ["sales_order",
-                    "design",
-                    "currency",
-                    "staff",
-                    "counterparty",
-                    "address",
-                    "department",
-                    "purchase_order",
-                    "payment_type",
-                    "payment",
-                    "transaction"]
+
+    known_tables = [
+        "sales_order",
+        "design",
+        "currency",
+        "staff",
+        "counterparty",
+        "address",
+        "department",
+        "purchase_order",
+        "payment_type",
+        "payment",
+        "transaction",
+    ]
 
     for table in known_tables:
         df = extract_data(secret, table, timestamp=timestamp)
 
         save_dataframe_to_s3_parquet(
-            dataframe=df,
-            bucket_name=os.environ["INGEST_BUCKET"],
-            table_name=table
+            dataframe=df, bucket_name=os.environ["INGEST_BUCKET"], table_name=table
         )
--- tests/test_extract_data.py
+++ tests/test_extract_data.py
@@ -3,13 +3,18 @@ import pandas as pd
 from unittest import mock
 from psycopg2 import OperationalError
 from pandas.errors import DatabaseError
-from ingestion.extract_data import extract_data, lambda_handler, save_dataframe_to_s3_parquet
+from ingestion.extract_data import (
+    extract_data,
+    lambda_handler,
+    save_dataframe_to_s3_parquet,
+)
 import boto3
 import pytest
 from moto import mock_aws
-from pandas.testing import assert_frame_equal 
+from pandas.testing import assert_frame_equal
 from io import BytesIO
 from datetime import datetime as real_datetime
+
 
 @mock.patch("ingestion.extract_data.pd.read_sql_query")
 @mock.patch("ingestion.extract_data.psycopg2.connect")
@@ -19,22 +24,19 @@ def test_extract_data(mock_connect, mock_read_sql_query):
         "password": "test_password",
         "dbname": "test_db",
         "host": "test_host",
-        "port": 5432
+        "port": 5432,
     }
 
     mock_connect.return_value = mock.MagicMock()
-    mock_read_sql_query.return_value = pd.DataFrame({
-        "last_updated": ["2026-01-02 10:00:00"]
-    })
+    mock_read_sql_query.return_value = pd.DataFrame(
+        {"last_updated": ["2026-01-02 10:00:00"]}
+    )
 
     result = extract_data(
-        secrets=secrets,
-        table_name="sales_order",
-        timestamp="2026-01-01 00:00:00"
+        secrets=secrets, table_name="sales_order", timestamp="2026-01-01 00:00:00"
     )
 
     assert isinstance(result, pd.DataFrame)
-
 
 
 # sad path
@@ -44,9 +46,8 @@ def test_extract_data_raises_exception():
         "password": "test_password",
         "dbname": "test_db",
         "host": "test.host",
-        "port": 5432
+        "port": 5432,
     }
-
 
     with pytest.raises(OperationalError):
         extract_data(secrets, table_name="sales_order", timestamp="2026-01-01 00:00:00")
@@ -59,13 +60,16 @@ def test_save_dataframe_to_s3_parquet():
     # create a bucket
     s3_client.create_bucket(Bucket="mock-s3")
     mock_data = {
-            "secrets":["secrets"],
-            "table_name":["sales_order"],
-            "timestamp":["2026-01-01 00:00:00"],
+        "secrets": ["secrets"],
+        "table_name": ["sales_order"],
+        "timestamp": ["2026-01-01 00:00:00"],
     }
     mock_df = pd.DataFrame(mock_data)
     filepath = save_dataframe_to_s3_parquet(mock_df, "mock-s3", "sales_order")
-    response = s3_client.get_object(Bucket="mock-s3", Key=filepath,)
+    response = s3_client.get_object(
+        Bucket="mock-s3",
+        Key=filepath,
+    )
     saved_df = pd.read_parquet(BytesIO(response["Body"].read()))
 
     assert_frame_equal(mock_df, saved_df)
@@ -82,36 +86,35 @@ def test_lambda_handler_extracts_all_data_when_bucket_is_empty(
     mock_save_dataframe,
     monkeypatch,
 ):
-        monkeypatch.setenv("INGEST_BUCKET", "mock-s3")
+    monkeypatch.setenv("INGEST_BUCKET", "mock-s3")
 
-        mock_s3_client = mock.MagicMock()
-        mock_boto_client.return_value = mock_s3_client
+    mock_s3_client = mock.MagicMock()
+    mock_boto_client.return_value = mock_s3_client
 
-        mock_s3_client.list_objects_v2.return_value = {
-            "KeyCount": 0
-        }
+    mock_s3_client.list_objects_v2.return_value = {"KeyCount": 0}
 
-        mock_get_secret.return_value = {
-            "username": "test_user",
-            "password": "test_password",
-            "dbname": "test_db",
-            "host": "test_host",
-            "port": 5432,
-        }
+    mock_get_secret.return_value = {
+        "username": "test_user",
+        "password": "test_password",
+        "dbname": "test_db",
+        "host": "test_host",
+        "port": 5432,
+    }
 
-        mock_extract_data.return_value = pd.DataFrame()
+    mock_extract_data.return_value = pd.DataFrame()
 
-        lambda_handler({}, None)
+    lambda_handler({}, None)
 
-        # store the args passed to the mock_extract_data function
-        # for the first table 'sales_order' which is index [0]
-        first_call = mock_extract_data.call_args_list[0]
+    # store the args passed to the mock_extract_data function
+    # for the first table 'sales_order' which is index [0]
+    first_call = mock_extract_data.call_args_list[0]
 
-        #assert that the keyword arg for the mock_extract_data function
-        #'timestamp' is a None value
-        assert first_call.kwargs["timestamp"] == None
-        assert mock_extract_data.call_count == 11
-        assert mock_save_dataframe.call_count == 11
+    # assert that the keyword arg for the mock_extract_data function
+    #'timestamp' is a None value
+    assert first_call.kwargs["timestamp"] == None
+    assert mock_extract_data.call_count == 11
+    assert mock_save_dataframe.call_count == 11
+
 
 @mock.patch("ingestion.extract_data.save_dataframe_to_s3_parquet")
 @mock.patch("ingestion.extract_data.extract_data")
@@ -131,9 +134,7 @@ def test_lambda_handler_uses_timestamp_when_bucket_contains_data(
     mock_s3_client = mock.MagicMock()
     mock_boto_client.return_value = mock_s3_client
 
-    mock_s3_client.list_objects_v2.return_value = {
-        "KeyCount": 11
-    }
+    mock_s3_client.list_objects_v2.return_value = {"KeyCount": 11}
 
     mock_get_secret.return_value = {
         "username": "test_user",
