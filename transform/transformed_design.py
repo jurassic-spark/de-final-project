9536beaca0b9143837fc1e70914698f534e7775b
Mohammedal-abdulhamid <mohammedalabdulhamid@gmail.com>
2026-08-17 15:55:18 +0100

    general function for read_tables

--- transform/transformed_design.py
+++ transform/transformed_design.py
@@ -1,24 +1,25 @@
 import pandas as pd
-import fsspec
+import fsspec 
 import s3fs
+from transform.read_table import read_table
 
 
-def transformed_design():
 
-    bucket = "js-final-proj-ingested-194169601943-dev"
-    df = pd.read_parquet(
-        f"s3://{bucket}"
-    )
 
+def transformed_design(table):
+    # read the table
+    df = read_table(table)
+
+    # drop unwanted columns
     df = df.drop(
-        columns=['created_at', 'last_updated']
+    columns=['created_at','last_updated']
     )
-
+    # drop duplicate
     df = df.drop_duplicates(
-        subset='design_id',
-        keep='last'
+    subset='design_id',
+    keep='last'
     )
     print(df.head())
 
 
-transformed_design()
+transformed_design('design')
