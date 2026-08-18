5d87e6b730857fe5fc921325dd2c3182b419716f
genevievehg <genevievegarwood@gmail.com>
2026-08-18 10:39:37 +0100

    feat: add log metric filter and cloudwatch alarm provisioning

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -1,7 +1,33 @@
 resource "aws_cloudwatch_log_group" "ingest_function_log_group" {
-  name              = "/aws/lambda/ingest_function_log"
+  name              = "/aws/lambda/ingest_raw_data"
   retention_in_days = 30
   lifecycle {
     prevent_destroy = false
   }
+}
+
+resource "aws_cloudwatch_log_metric_filter" "ingest_function_error" {
+  name           = "ingest_function_error"
+  pattern        = "ERROR"
+  log_group_name = "/aws/lambda/ingest_raw_data"
+
+  metric_transformation {
+    name      = "ingest_function_error"
+    namespace = "Lambda errors"
+    value     = "1"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "ingest_function_error_alarm" {
+  alarm_name                = "ingest_function_error_alarm"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = 1
+  metric_name               = aws_cloudwatch_log_metric_filter.ingest_function_error.name
+  namespace                 = "Lambda errors"
+  period                    = 60
+  statistic                 = "Sum"
+  threshold                 = 1
+  alarm_description         = "Alert when ingest function logs contain Error"
+  insufficient_data_actions = []
+  alarm_actions = [aws_sns_topic.error_notification.arn]
 }
