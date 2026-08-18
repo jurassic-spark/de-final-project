c3231639d0075201ffb5a4073438c0e7cab7e8e2
Mohammedal-abdulhamid <mohammedalabdulhamid@gmail.com>
2026-08-18 10:54:10 +0100

    read data from s3 function

--- transform/read_table.py
+++ transform/read_table.py
@@ -6,7 +6,7 @@ import os
 
 
 
-def read_table(table):
+def read_tables_from_s3(table):
     bucket = os.environ['S3_INGESTION_BUCKET'] # export s3 name in CLI
     df = pd.read_parquet(
         f's3://{bucket}/raw/{table}')
